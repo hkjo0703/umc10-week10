a1961b1b40f24c825021097c0695ec304e7336bb
hkjo0703 <gkrua0703@naver.com>
2026-05-31 16:45:02 +0900

    fix: remove old router files

--- week10/src/modules/store/store.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { handleAddStore } from "./controller/store.controller.js";
-
-const router = Router();
-
-router.post("/", handleAddStore);
-
-export default router;
